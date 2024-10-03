24cba43518b9b1a66630c800dc733ffca5d9eb6b
Bruno Díaz Martín <brunodiazmartin5@gmail.com>
2024-10-03 15:46:29 +0100

    Modularized. Added Hilt

---
+++ app/src/main/java/com/nebulavision/pokedexcompose/ui/theme/Color.kt
@@ -0,0 +1,60 @@
+package com.nebulavision.pokedexcompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+// Colores para cada tipo de Pokémon
+val colorMap = mapOf(
+    "type_normal" to Color(0xFFA0A2A0),
+    "type_fire" to Color(0xFFF7786B),
+    "type_water" to Color(0xFF2481F0),
+    "type_electric" to Color(0xFFFAC100),
+    "type_grass" to Color(0xFF4FC1A6),
+    "type_ice" to Color(0xFF3DD9FF),
+    "type_fighting" to Color(0xFFFF8100),
+    "type_poison" to Color(0xFF923FCC),
+    "type_ground" to Color(0xFF92501B),
+    "type_flying" to Color(0xFF82BAF0),
+    "type_psychic" to Color(0xFFF95587),
+    "type_bug" to Color(0xFF92A212),
+    "type_rock" to Color(0xFFB6A136),
+    "type_ghost" to Color(0xFF735797),
+    "type_dragon" to Color(0xFF4F60E2),
+    "type_dark" to Color(0xFF4F3F3D),
+    "type_steel" to Color(0xFF60A2B9),
+    "type_fairy" to Color(0xFFD685AD)
+)
+
+
+// Colores del tema
+val md_theme_light_primary = Color(0xFFF44336) // Rojo
+val md_theme_light_onPrimary = Color(0xFFFFFFFF) // Blanco
+val md_theme_light_primaryContainer = Color(0xFFFFCDD2) // Rojo claro
+val md_theme_light_onPrimaryContainer = Color(0xFFB71C1C) // Rojo oscuro
+
+val md_theme_light_secondary = Color(0xFF4CAF50) // Verde
+val md_theme_light_onSecondary = Color(0xFFFFFFFF) // Blanco
+val md_theme_light_secondaryContainer = Color(0xFFD8EAD9) // Verde claro
+val md_theme_light_onSecondaryContainer = Color(0xFF1B5E20) // Verde oscuro
+
+val md_theme_light_background = Color(0xFFFFFFFF) // Blanco
+val md_theme_light_onBackground = Color(0xFF1C1B1F) // Tono oscuro
+
+val md_theme_light_surface = Color(0xFFF8F9FA) // Gris claro
+val md_theme_light_onSurface = Color(0xFF1C1B1F) // Tono oscuro
+
+val md_theme_dark_primary = Color(0xFFE57373) // Rojo claro
+val md_theme_dark_onPrimary = Color(0xFFB71C1C) // Rojo oscuro
+val md_theme_dark_primaryContainer = Color(0xFFB71C1C) // Rojo profundo
+val md_theme_dark_onPrimaryContainer = Color(0xFFE57373) // Color claro
+
+val md_theme_dark_secondary = Color(0xFF81C784) // Verde claro
+val md_theme_dark_onSecondary = Color(0xFF1B5E20) // Verde oscuro
+val md_theme_dark_secondaryContainer = Color(0xFF388E3C) // Verde intenso
+val md_theme_dark_onSecondaryContainer = Color(0xFF81C784) // Color claro
+
+val md_theme_dark_background = Color(0xFF121212) // Negro
+val md_theme_dark_onBackground = Color(0xFFE6E1E5) // Gris claro
+
+val md_theme_dark_surface = Color(0xFF121212) // Negro
+val md_theme_dark_onSurface = Color(0xFFE6E1E5) // Gris claro
